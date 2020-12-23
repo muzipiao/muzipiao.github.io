3998f69c04186a7f6d14dca7e2a73e2a918bdd7c
lifei <lifei_zdjl@126.com>
2020-12-23 18:39:21 +0800

    test actions.

--- status_email.py
+++ status_email.py
@@ -15,10 +15,6 @@ mail_user = str(sys.argv[1])
 mail_pass = str(sys.argv[2])
 status_code = int(sys.argv[3])
 
-user_name = os.popen("echo $username").read()
-print("用户名：" + str(user_name))
-print("用户名：" + mail_user)
-
 if status_code == 301 or status_code == 200:
     print("网站状态正常" + str(status_code))
     sys.exit(0)
@@ -35,6 +31,6 @@ try:
     smtpObj.connect(mail_host, 587)
     smtpObj.login(mail_user,mail_pass)
     smtpObj.sendmail(sender, receivers, message.as_string())
-    print("邮件发送成功")
+    print("邮件发送成功，状态码：" + str(status_code))
 except smtplib.SMTPException:
     print("无法发送邮件，原因：\n" + str(smtplib.Exception))
